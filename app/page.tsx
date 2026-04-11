060a7e6c416fff8c7983960a1fe414c4e0df55a8
Jerry Lee Melton <jerryleemelton@gmail.com>
2026-04-10 23:28:09 -0700

    Implement 3 oscillator synth and a set of simple keyboard buttons to control them.

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,42 @@
 "use client"
 
 import { useEffect, useRef, useState } from "react"
+import { useAudio } from "@/audio/AudioProvider"
+
+const keyboardKeys = [
+  { note: 60, label: "C" },
+  { note: 62, label: "D" },
+  { note: 64, label: "E" },
+  { note: 65, label: "F" },
+  { note: 67, label: "G" },
+  { note: 69, label: "A" },
+  { note: 71, label: "B" },
+  { note: 72, label: "C" },
+]
 
 export default function Home() {
-  const audioContextRef = useRef<AudioContext | null>(null)
-  const oscOneRef = useRef<OscillatorNode | null>(null)
-  const gainRef = useRef<GainNode | null>(null)
-  const [oscPlaying, setOscPlaying] = useState<boolean>(false)
-
-  useEffect(() => {
-    const audioContext = new AudioContext()
-    const oscOne = audioContext.createOscillator()
-    const gain = audioContext.createGain()
-
-    oscOne.type = "sine"
-    oscOne.frequency.value = 440
-    gain.gain.value = 0
-
-    oscOne.connect(gain).connect(audioContext.destination)
-    oscOne.start()
-
-    audioContextRef.current = audioContext
-    oscOneRef.current = oscOne
-    gainRef.current = gain
-
-    return () => {
-      oscOne.stop()
-      audioContext.close()
-    }
-  }, [])
-
-  function toggleOscillator(): void {
-    console.log("Starting or Stopping osc1")
-
-    const audioContext = audioContextRef.current
-    const gain = gainRef.current
-
-    if (!audioContext || !gain) return
-
-    audioContext.resume()
-
-    if (!oscPlaying) {
-      gain.gain.setValueAtTime(1, audioContext.currentTime)
-    } else {
-      gain.gain.setValueAtTime(0, audioContext.currentTime)
-    }
-
-    setOscPlaying(!oscPlaying)
-  }
-
+  const { playNote, stopNote } = useAudio()
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <button onClick={toggleOscillator}>Toggle Oscillator</button>
+      <div className="keyboard-container">
+        {keyboardKeys.map(({ note, label }) => (
+          <button
+            key={note}
+            onMouseDown={() => {
+              playNote(note)
+            }}
+            onMouseUp={() => {
+              stopNote(note)
+            }}
+            onMouseLeave={() => {
+              stopNote(note)
+            }}
+            className={"keyboard-key"}
+          >
+            {label}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
--- audio/AudioProvider.tsx
+++ audio/AudioProvider.tsx
@@ -3,13 +3,25 @@
 import { createContext, useContext, useRef, ReactNode } from "react"
 
 type AudioContextType = {
-  getAudioContext: () => AudioContext
+  // getAudioContext: () => AudioContext
+  playNote: (note: number) => void
+  stopNote: (note: number) => void
+}
+
+type Voice = {
+  oscillators: OscillatorNode[]
+  gain: GainNode
 }
 
 const AudioCtx = createContext<AudioContextType | null>(null)
 
+function midiToFreqency(note: number) {
+  return 440 * Math.pow(2, (note - 69) / 12)
+}
+
 export default function AudioProvider({ children }: { children: ReactNode }) {
   const audioContextRef = useRef<AudioContext | null>(null)
+  const voicesRef = useRef<Map<number, Voice>>(new Map())
 
   function getAudioContext() {
     if (!audioContextRef.current) {
@@ -19,8 +31,76 @@ export default function AudioProvider({ children }: { children: ReactNode }) {
     return audioContextRef.current
   }
 
+  function playNote(note: number): void {
+    if (voicesRef.current.has(note)) {
+      return // Return early, note already playing somehow
+    }
+
+    // Get an initialize audio context
+    const audioContext = getAudioContext()
+    audioContext.resume()
+
+    // Set the note's frequency and gain
+    const noteFrequency = midiToFreqency(note)
+    const gain = audioContext.createGain()
+    gain.gain.setValueAtTime(0, audioContext.currentTime)
+    // Short default ramp to avoid pops
+    gain.gain.linearRampToValueAtTime(0.3, audioContext.currentTime + 0.02)
+    gain.connect(audioContext.destination)
+
+    // Create oscillators and give them the note data
+    const osc1 = audioContext.createOscillator()
+    osc1.type = "sawtooth"
+    osc1.frequency.value = noteFrequency
+    osc1.connect(gain)
+
+    const osc2 = audioContext.createOscillator()
+    osc2.type = "sawtooth"
+    osc2.frequency.value = noteFrequency
+    osc2.detune.value = 5
+    osc2.connect(gain)
+
+    const osc3 = audioContext.createOscillator()
+    osc2.type = "sawtooth"
+    osc2.frequency.value = noteFrequency
+    osc2.detune.value = 5
+    osc2.connect(gain)
+
+    // Play the note!
+    osc1.start()
+    osc2.start()
+    osc3.start()
+
+    // Add note to the voiceRef for tracking and later stopping
+    voicesRef.current.set(note, { oscillators: [osc1, osc2, osc3], gain })
+  }
+
+  function stopNote(note: number): void {
+    const voice = voicesRef.current.get(note)
+
+    if (!voice) {
+      return // Invalid voice, return
+    }
+
+    const audioContext = getAudioContext()
+    const now = audioContext.currentTime
+    const releaseTime = 0.05
+
+    // Short default release value to avoid pops
+    voice.gain.gain.setValueAtTime(voice.gain.gain.value, now)
+    voice.gain.gain.linearRampToValueAtTime(0, now + releaseTime)
+
+    // Stop oscillators after release finishes
+    setTimeout(() => {
+      voice.oscillators.forEach((oscillator) => oscillator.stop())
+      voice.gain.disconnect()
+    })
+
+    voicesRef.current.delete(note)
+  }
+
   return (
-    <AudioCtx.Provider value={{ getAudioContext }}>
+    <AudioCtx.Provider value={{ playNote, stopNote }}>
       {children}
     </AudioCtx.Provider>
   )
